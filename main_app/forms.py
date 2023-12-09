a637592ac87c7399c5ea4789609b2e9ab3cd415b
Asimmons8228 <96853510+Asimmons8228@users.noreply.github.com>
2023-12-09 13:11:46 -0500

    configured fields for user to input location

--- main_app/forms.py
+++ main_app/forms.py
@@ -8,3 +8,23 @@ class UserForm(UserCreationForm):
     last_name = forms.CharField(max_length=30, required=True, help_text='Required.')
     email = forms.EmailField(max_length=254, help_text='Required. Enter a valid email address.')
     state = forms.CharField(max_length=255)
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email', 'username', 'password1', 'password2']
+
+    def save(self, commit=True):
+        user = super(UserForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+
+        if commit:
+            user.save()
+
+            location = Location.objects.create(
+                state=self.cleaned_data['state']
+            )
+
+            user.profile.location = location
+            user.profile.save()
+
+        return user
