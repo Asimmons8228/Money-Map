6367158a9984e7e0d00f0d26f9c6fa507d0915a8
Asimmons8228 <96853510+Asimmons8228@users.noreply.github.com>
2023-12-09 12:58:52 -0500

    set up imports in views folder

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django import forms
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .finhealth import get_finhealth
+from .models import Bill, User, Income, Expense, FinancialHealth, Location
+from .forms import UserForm, Profile
 
 
