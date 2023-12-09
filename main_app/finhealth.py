7205364e4d092aa95b294955ce7c1061b6db6dd0
Asimmons8228 <96853510+Asimmons8228@users.noreply.github.com>
2023-12-09 13:20:21 -0500

    added formulas to detemine financial health score based on percentages of savings, nonessentials, essentials, and needs

--- main_app/finhealth.py
+++ main_app/finhealth.py
@@ -27,3 +27,19 @@ def get_finhealth(user):
   nonessential_expenses = Expense.objects.filter(user =  user, category = 'Nonessential')
   total_essential_expenses = sum(expense.amount for expense in essential_expenses)
   total_nonessential_expenses = sum(expense.amount for expense in nonessential_expenses)
+
+  non_essential_spending = (total_nonessential_bills + total_nonessential_expenses) * 12
+  essential_spending = (total_essential_bills + total_essential_expenses) * 12
+  savings = yearly_income - (yearly_bills + yearly_estimated_expenses)
+
+  needs_percent = (yearly_bills + yearly_estimated_expenses) / yearly_income * 100
+  savings_percent = ((yearly_income - (yearly_bills + yearly_estimated_expenses)) / yearly_income) * 100
+  nonessential_percent = (non_essential_spending / yearly_income) * 100
+  essential_percent = (essential_spending / yearly_income) * 100
+
+  needs_score = calculate_score(needs_percent, needs_ranges)
+  essential_score = calculate_score(essential_percent, essential_ranges)
+  nonessential_score = calculate_score(nonessential_percent, nonessential_ranges)
+  savings_score= calculate_score( savings_percent, savings_ranges) 
+
+
