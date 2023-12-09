46b39032e5f7ae791017f01c827c82cd40e47e99
Asimmons8228 <96853510+Asimmons8228@users.noreply.github.com>
2023-12-09 15:01:08 -0500

    Styled Sign Up page and Login Page

--- main_app/forms.py
+++ main_app/forms.py
@@ -4,9 +4,9 @@ from django.contrib.auth.models import User
 from .models import Location, Profile
 
 class UserForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30, required=True, help_text='Required.')
-    last_name = forms.CharField(max_length=30, required=True, help_text='Required.')
-    email = forms.EmailField(max_length=254, help_text='Required. Enter a valid email address.')
+    first_name = forms.CharField(max_length=30, required=True,)
+    last_name = forms.CharField(max_length=30, required=True,)
+    email = forms.EmailField(max_length=254, )
     state = forms.CharField(max_length=255)
 
     class Meta:
